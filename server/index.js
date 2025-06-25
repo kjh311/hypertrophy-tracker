07b137d3b18416d612884abff4d186adf5537f51
kjh311 <kjh311@gmail.com>
2025-06-24 18:44:30 -0600

    mongodb connected

---
+++ server/index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      //   "https://budget-tracker-mern.netlify.app",
+      //   "https://mern-expense-tracker.fly.dev",
+    ],
+    credentials: true,
+  })
+);
+
+// app.options("*", cors());
+//delete
+
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+// const expensesRoutes = require("./routes/expenses");
+// const subcategoriesRoute = require("./routes/subcategories");
+// const transactionRoutes = require("./routes/transactions");
+// const budgetRoutes = require("./routes/budget");
+// const exportRoutes = require("./routes/exportRoutes");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+// app.use("/api/expenses", expensesRoutes);
+// app.use("/api/subcategories", subcategoriesRoute);
+// app.use("/api/transactions", transactionRoutes);
+// app.use("/api/budget", budgetRoutes);
+// app.use("/api/export", exportRoutes);
+
+// serviceWorkerRegistration.register();
+
+// connect to mongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`✅ Connected to DB: ${mongoose.connection.name}`))
+  .catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("API is running!!!");
+});
+
+//Ping Render to keep from cold sleep
+// app.get("/api/ping", (req, res) => {
+//   res.send("pong");
+// });
+
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server is running on port ${PORT}`)
+);
---
+++ server/routes/auth.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const verifyToken = require("../middleware/verifyToken");
+
+const router = express.Router();
+
+//REGISTER NEW USER
+router.post("/register", async (req, res) => {
+  const { name, userName, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ userName });
+    if (existingUser)
+      return res.status(409).json({ message: "User already exists!" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      name,
+      userName,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "365d",
+    });
+
+    res.status(201).json({
+      message: "Registration successful",
+      token,
+      user: { id: newUser._id, name: newUser.name, userName: newUser.userName },
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+
+  // After user is created:
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { userName, password } = req.body;
+
+  try {
+    const user = await User.findOne({ userName });
+    if (!user) return res.status(404).json({ message: "User not found!" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "365d",
+    });
+
+    res.status(200).json({ message: "Login successful", user, token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Protected Routes
+router.get("/welcome", verifyToken, (req, res) => {
+  res.json({ message: "Welcome to your dashboard" });
+});
+
+module.exports = router;
